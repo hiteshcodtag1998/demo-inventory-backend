267bcd5147c5905a361615ec654facc67c9c5965
Hitesh Rajput <hitesh@codtag.com>
2024-04-02 21:36:06 +0530

    Fix: PDF download issue on server

--- controller/purchase.js
+++ controller/purchase.js
@@ -243,8 +243,9 @@ const updateSelectedPurchaase = async (req, res) => {
   }
 };
 
-const purchasePdfDownload = (req, res) => {
+const purchasePdfDownload = async (req, res) => {
   try {
+    console.log('req.body', req.body)
     const payload = {
       title: "Purchase Note",
       supplierName: req.body?.SupplierName || "",
@@ -255,7 +256,7 @@ const purchasePdfDownload = (req, res) => {
       referenceNo: req.body?.referenceNo || ""
     }
     // Usage
-    generatePDFfromHTML(invoiceBill(payload), res);
+    await generatePDFfromHTML(invoiceBill(payload), res);
   } catch (error) {
     console.log('error in productPdfDownload', error)
   }
